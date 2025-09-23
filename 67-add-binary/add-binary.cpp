dcc43982616313bde8f6a3c8a8fd48758dfef842
Abhishek kumar <ak95722352@gmail.com>
2025-09-23 10:40:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (38.90%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -5,24 +5,21 @@ public:
         int carry=0; 
         string s;
         while(i>=0 && j>=0){
-            int sum=(a[i]-'0')+(b[j]-'0')+carry;
-                int a=sum%2;
-                s.insert(s.begin(),char(a + '0'));
+                int sum=(a[i]-'0')+(b[j]-'0')+carry;
+                s.insert(s.begin(),char(sum%2 + '0'));
                 carry=sum/2;
                 i--;
                 j--;
             }
         while(i>=0){
                 int sum=(a[i]-'0')+carry;
-                int a=sum%2;
-                s.insert(s.begin(),char(a + '0'));
+                s.insert(s.begin(),char(sum%2 + '0'));
                 carry=sum/2;
                 i--;
         }
         while(j>=0){
                 int sum=(b[j]-'0')+carry;
-                int a=sum%2;
-                s.insert(s.begin(),char(a + '0'));
+                s.insert(s.begin(),char(sum%2 + '0'));
                 carry=sum/2;
                 j--;
         }
