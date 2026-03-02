16c44beab668d23b14fec68722f48b12e9bb400c
Abhishek kumar <ak95722352@gmail.com>
2026-03-02 08:33:16 +0530

    Time: 3 ms (40.00%) | Memory: 10 MB (30.89%) - LeetSync

---
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxDifference(string s) {
+        unordered_map<char,int> mp;
+
+        for(char ch : s) {
+            mp[ch]++;
+        }
+
+        int maxOdd = INT_MIN;
+        int minEven = INT_MAX;
+
+        for(auto x : mp) {
+            if(x.second % 2 == 1) {   
+                maxOdd = max(maxOdd, x.second);
+            }
+            else {                    
+                minEven = min(minEven, x.second);
+            }
+        }
+
+        if(maxOdd == INT_MIN || minEven == INT_MAX)
+            return -1;  
+
+        return maxOdd - minEven;
+    }
+};
