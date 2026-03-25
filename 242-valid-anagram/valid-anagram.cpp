730805b1a44f50896be7c11e1c03718871428177
Abhishek kumar <ak95722352@gmail.com>
2026-03-26 00:46:45 +0530

    Time: 5 ms (27.31%) | Memory: 9.9 MB (23.77%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int l1=s.length();
-        int l2=t.length();
-        if(l1!=l2) return false;
-        int a[26]={0};
-        int b[26]={0};
-        for(int i=0;i<l1;i++){
-            a[s[i]-'a']= a[s[i]-'a']+1;
-            b[t[i]-'a']= b[t[i]-'a']+1;
-        }
-        for(int i=0;i<26;i++){
-            if(a[i]!=b[i])
-                return false;
+        if(s.size() != t.size()) return false;
+        unordered_map<char,int>mp1,mp2;
+        for(int i = 0; i < s.size(); i++)
+            {
+                mp1[s[i]]++;
+                mp2[t[i]]++;
+            }
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(mp1[s[i]] != mp2[s[i]]) return false;
         }
         return true;
-        
-        
     }
 };
