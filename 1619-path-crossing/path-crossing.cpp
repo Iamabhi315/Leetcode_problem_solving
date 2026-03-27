2875a47797fa6c67a19af6f9629b53d5256adccd
Abhishek kumar <ak95722352@gmail.com>
2026-03-27 22:31:21 +0530

    Time: 3 ms (15.49%) | Memory: 10.8 MB (24.96%) - LeetSync

--- 1619-path-crossing/path-crossing.cpp
+++ 1619-path-crossing/path-crossing.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     bool isPathCrossing(string path) {
         int x = 0, y = 0;
-        unordered_map<string, bool> mp;
+        unordered_set<string> st;
 
-        mp["0,0"] = true;
+        st.insert("0,0");
 
-        for(int i = 0; i < path.size(); i++) {
-            if(path[i] == 'N') y++;
-            else if(path[i] == 'S') y--;
-            else if(path[i] == 'E') x++;
+        for(char c : path) {
+            if(c == 'N') y++;
+            else if(c == 'S') y--;
+            else if(c == 'E') x++;
             else x--;
 
             string key = to_string(x) + "," + to_string(y);
 
-            if(mp.find(key) != mp.end())
+            if(st.count(key))
                 return true;
 
-            mp[key] = true;
+            st.insert(key);
         }
         return false;
     }
