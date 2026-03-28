bad168fb2948ca22dcc4793025a51b909b95559e
Abhishek kumar <ak95722352@gmail.com>
2026-03-29 02:08:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (75.54%) - LeetSync

--- 1044-find-common-characters/find-common-characters.cpp
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
-        vector<int> freq(26, 0);
-        for(char c : words[0]) {
-            freq[c - 'a']++;
+    vector<int> freq(26, INT_MAX);
+
+    for(string &word : words) {
+        vector<int> temp(26, 0);
+
+        for(char c : word) {
+            temp[c - 'a']++;
         }
-        vector<int> temp;
-        for(int i = 0; i < words.size(); i++){
-            temp = freq;
-            fill(freq.begin(), freq.end(), 0);
-            for(int j = 0; j < words[i].size(); j++){
-                if(temp[words[i][j] - 'a'] > 0){
-                    temp[words[i][j] - 'a']--;
-                    freq[words[i][j] - 'a']++;
-                }
-            }
-        }
-        vector<string> v;
 
         for(int i = 0; i < 26; i++) {
-            while(freq[i] > 0) {
-                string temp(1, char(i + 'a'));
-                v.push_back(temp);
-                freq[i]--;
-            }
+            freq[i] = min(freq[i], temp[i]);
         }
-        return v;
     }
+
+    vector<string> res;
+
+    for(int i = 0; i < 26; i++) {
+        while(freq[i] > 0) {
+            res.push_back(string(1, i + 'a'));
+            freq[i]--;
+        }
+    }
+
+    return res;
+}
 };
