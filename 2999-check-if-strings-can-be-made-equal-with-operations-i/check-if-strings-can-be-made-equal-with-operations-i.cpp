38c258ba242bd75ab3338617174839ead204b2cb
Abhishek kumar <ak95722352@gmail.com>
2026-03-29 22:24:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (38.71%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -3,6 +3,6 @@ public:
     bool canBeEqual(string s1, string s2) {
         if(s1[0] == s2[2]) swap(s1[0] , s1[2]);
         if(s1[1] == s2[3]) swap(s1[1] , s1[3]);
-        return s1 == s2? true : false;
+        return s1 == s2;
     }
 };
