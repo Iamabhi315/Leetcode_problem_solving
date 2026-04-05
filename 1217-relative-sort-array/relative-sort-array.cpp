01bfadf13c0dcca668b6c88dd2e3aa5309f3903f
Abhishek kumar <ak95722352@gmail.com>
2026-04-05 12:40:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (20.54%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        unordered_map<int , int>mp;
+        map<int , int>mp;
         vector<int>res;
         for(int i = 0; i < arr1.size(); i++) mp[arr1[i]]++;
         for(int i = 0; i < arr2.size(); i++){
             while(mp[arr2[i]] > 0){
                 res.push_back(arr2[i]);
                 mp[arr2[i]]--;
+            }
         }
-        }
-        vector<int>temp;
         for(auto &p : mp){
             int freq = p.second;
             while(freq--){
-                temp.push_back(p.first);
+                res.push_back(p.first);
             }
         }
-        sort(temp.begin(), temp.end());
-        for(int i = 0; i < temp.size(); i++) res.push_back(temp[i]);
         return res;
     }
 };
