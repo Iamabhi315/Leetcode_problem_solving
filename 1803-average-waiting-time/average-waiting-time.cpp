c000dbc8a6c2cb0beb84448a05fb86c49f5d3c6d
Abhishek kumar <ak95722352@gmail.com>
2026-04-05 08:19:28 +0530

    Time: 0 ms (100.00%) | Memory: 77.2 MB (48.15%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
-        int t_completion = customers[0][0];
-        int n = customers.size();
-        double avg = 0;
-        for(int i = 0; i < n; i++){
-            if(t_completion >= customers[i][0]){
-                t_completion += customers[i][1];
-                avg += (t_completion - customers[i][0]);
-            }
-            else{
-                t_completion = customers[i][0] + customers[i][1] ;
-                avg += t_completion - customers[i][0];
-            }
+        int t = 0;
+        double total = 0;
+
+        for(auto &c : customers){
+            t = max(t, c[0]);     // wait if needed
+            t += c[1];            // cooking
+            total += (t - c[0]);  // waiting time
         }
-        return avg/(double)n;
+
+        return total / customers.size();
     }
 };
