03a39e15e436db759bb280222aac477be03c29e1
Abhishek kumar <ak95722352@gmail.com>
2026-04-06 07:55:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (72.51%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        vector<int> freq(26, 0);
+
+        for(char c : s) {
+            freq[c - 'a']++;
+        }
+
+        string res;
+
+        for(char c : order) {
+            while(freq[c - 'a'] > 0) {
+                res.push_back(c);
+                freq[c - 'a']--; 
+            }
+        }
+
+        for(int i = 0; i < 26; i++) {
+            while(freq[i] > 0) {
+                res.push_back(i + 'a');
+                freq[i]--; 
+            }
+        }
+
+        return res;
+    }
+};
