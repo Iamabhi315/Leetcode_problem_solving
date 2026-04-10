5fb63afa958d427f7ed9b0da4b82dcdf6c5a3901
Abhishek kumar <ak95722352@gmail.com>
2026-04-10 08:13:54 +0530

    Time: 52 ms (68.49%) | Memory: 79.1 MB (51.90%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-    void heapify(vector<int>& nums, int n, int i) {
-        int largest = i;
-        int left = 2*i + 1;
-        int right = 2*i + 2;
-
-        if(left < n && nums[left] > nums[largest])
-            largest = left;
-
-        if(right < n && nums[right] > nums[largest])
-            largest = right;
-
-        if(largest != i) {
-            swap(nums[i], nums[largest]);
-            heapify(nums, n, largest);
-        }
-    }
-
     vector<int> sortArray(vector<int>& nums) {
-        int n = nums.size();
-        for(int i = n/2 - 1; i >= 0; i--) {
-            heapify(nums, n, i);
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(int i = 0; i < nums.size(); i++) {
+            pq.push(nums[i]);
         }
 
-        for(int i = n - 1; i > 0; i--) {
-            swap(nums[0], nums[i]); 
-            heapify(nums, i, 0); 
+        vector<int> res;
+
+        while(!pq.empty()) {
+            res.push_back(pq.top());
+            pq.pop();
         }
 
-        return nums;
+        return res;
     }
 };
