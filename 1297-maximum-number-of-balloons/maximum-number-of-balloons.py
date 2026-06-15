5e68b0d342c4842dffe8f53b695f3a185971b460
Abhishek kumar <ak95722352@gmail.com>
2026-06-15 07:22:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (14.88%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,19 +1,10 @@
 class Solution(object):
     def maxNumberOfBalloons(self, text):
-        """
-        :type text: str
-        :rtype: int
-        """
-        count = defaultdict(int)
-
-        for ch in text:
-            count[ch] += 1
-
-        return min(
-            count['b'],
-            count['a'],
-            count['l'] // 2,
-            count['o'] // 2,
-            count['n']
-        )
+        b=text.count('b')
+        a=text.count('a')
+        l=text.count('l')//2
+        o=text.count('o')//2
+        n=text.count('n')
+        print(b,a,l,o,n)
+        return min(b,a,l,o,n)
         
