6e2921d8c25b898e36e6b863999fe59c7e1fd4d9
Abhishek kumar <ak95722352@gmail.com>
2026-06-16 23:21:18 +0530

    Time: 27 ms (32.99%) | Memory: 13.7 MB (22.20%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def findClosestElements(self, arr, k, x):
+        """
+        :type arr: List[int]
+        :type k: int
+        :type x: int
+        :rtype: List[int]
+        """
+        diff = []
+        for val in arr:
+            diff.append(abs(val - x))
+
+        l = 0
+        r = k - 1
+        Sum = 0
+        indx = 0
+
+        for i in range(k):
+            Sum += diff[i]
+
+        mini = 2147483647
+
+        while r < len(arr) - 1:
+            if mini > Sum:
+                mini = Sum
+                indx = l
+
+            Sum -= diff[l]
+            l += 1
+            r += 1
+            Sum += diff[r]
+
+        if mini > Sum:
+            mini = Sum
+            indx = l
+
+        return arr[indx:indx + k]
