b52a7155e8240b66b68a5f783030ae7b0061bc5e
Abhishek kumar <ak95722352@gmail.com>
2026-06-29 21:03:39 +0530

    Time: 1 ms (81.79%) | Memory: 19.3 MB (57.95%) - LeetSync

--- 441-arranging-coins/arranging-coins.py
+++ 441-arranging-coins/arranging-coins.py
@@ -1,11 +1,16 @@
 class Solution:
-    def arrangeCoins(self, n: int) -> int:
-        res = 0
-        i = 1
-        temp = n
-        while temp > 0:
-            temp -= (res+1)
-            if temp < 0:
-                return res
-            res += 1
-        return res
+    def arrangeCoins(self, n: int):
+        low, high = 0, n
+
+        while low <= high:
+            mid = (low + high) // 2
+            coins = mid * (mid + 1) // 2
+
+            if coins == n:
+                return mid
+            elif coins < n:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return high
